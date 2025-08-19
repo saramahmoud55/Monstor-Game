867127f0fb40b98e7a33618526644b278c1c1324
saramahmoud55 <saraelsadek55555@gmail.com>
2025-08-19 18:27:58 +0300

    adding score functions

--- monster/app.js
+++ monster/app.js
@@ -2,38 +2,77 @@ function getRandomNumber(min , max){
  return Math.floor( Math.random()* (max-min)) +min
 }
 
-const app =createApp({
+const app =Vue.createApp({
   data(){
     return{
         playerHealth:100,
         monsterHealth:100,
+        currentRound:0,
+        winner:null
+    }
+  },
+  watch:{
+    playerHealth(value){
+        if(value <= 0 && this.monsterHealth <= 0){
+            this.winner ="draw"
+        }else if(value <= 0){
+            this.winner= 'lost'
+        }
+    },
+    monsterHealth(value){
+        if(value <= 0 && this.playerHealth <= 0){
+            this.winner ="draw"
+
+        }else if(value <= 0){
+            this.winner= 'win'
+
+        }
+    },
+
+  },
+  computed:{
+    monsterBarStyle(){
+        return {width: this.monsterHealth+"%"}
+    },
+    playerBarStyle(){
+        return {width: this.playerHealth+"%"}
+    },
+    isspecialAttackReady(){
+        return  this.currentRound % 3 !== 0
     }
   },
   methods:{
     attackMonster(){
+        this.currentRound++;
        const attackValue =getRandomNumber(5,12);
         this.monsterHealth =this.monsterHealth - attackValue;
         this.attackPlayer();
     },
     attackPlayer(){
          const attackValue =getRandomNumber(8,12);
-        this.attackPlayer = this.attackPlayer -attackValue
+        this.playerHealth = this.playerHealth - attackValue
+    },
+    specialAttack(){
+        this.currentRound++;
+        const attackValue =getRandomNumber(10,25)
+        this.monsterHealth =this.monsterHealth - attackValue
+        this.attackPlayer();
+
+    },
+    healPlayer(){
+        const healValue=getRandomNumber(8,20);
+        if(this.playerHealth + healValue > 100){
+        this.playerHealth=100
+        }else{
+            this.playerHealth += healValue
+        };
+        this.attackPlayer();
     }
   }
 
     
 })
 app.mount('#game')
-
-
-
-
-
-
-
-
-
-
 
 
 
